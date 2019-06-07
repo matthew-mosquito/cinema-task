95f280d3cb372ca047a3e2308a93c13e777086c5
matthew-mosquito <matthew@mosquitodigital.co.uk>
2019-06-07 16:07:39 +0100

    Added the Films model with validation attributes

---
+++ Mosquito.CinemaTask/Models/FilmModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mosquito.CinemaTask.Models
+{
+    public class FilmModel
+    {
+
+        [Required]
+        [StringLength(40)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0, 10)]
+        public int Rating { get; set; }
+
+        [Required]
+        [Range(0.1, 6)]
+        public float Duration { get; set; }
+    }
+}
