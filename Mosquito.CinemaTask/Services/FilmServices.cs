c27d22a2848937a1f3ce9d3157e4c29ded107979
matthew-mosquito <matthew@mosquitodigital.co.uk>
2019-06-10 09:25:08 +0100

    Implemented the IFilmService interface. Will write the methods once the repository has been finished

---
+++ Mosquito.CinemaTask/Services/FilmServices.cs
@@ -0,0 +1,32 @@
+﻿using Mosquito.CinemaTask.Models;
+using Mosquito.CinemaTask.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mosquito.CinemaTask.Services
+{
+    public class FilmServices : IFilmService
+    {
+        public bool AddFilm(FilmModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteFilm(FilmModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditFilm(FilmModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<FilmModel> GetAllCalls()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
